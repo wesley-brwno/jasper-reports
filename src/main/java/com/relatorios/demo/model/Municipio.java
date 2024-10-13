60db82bc8f1359567945732e9f15df956b840dbe
Wesley Bruno <brunow@outlook.com>
2024-10-12 21:09:30 -0300

    End of chapter 2.4 missing files

---
+++ src/main/java/com/relatorios/demo/model/Municipio.java
@@ -0,0 +1,24 @@
+package com.relatorios.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Municipio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    @JoinColumn(name = "uf_id")
+    private Uf uf;
+    @Column(name = "população")
+    private Long populacao;
+}
